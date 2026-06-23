b910d31f4cf08689e6da9a01bfc70dddf44abe30
Ahmed Ayman <ahmed.ayman@example.com>
2026-06-23 23:13:22 +0300

    first commit

---
+++ src/main/java/com/fincore/fincorebank/FincorebankApplication.java
@@ -0,0 +1,13 @@
+package com.fincore.fincorebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincorebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincorebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fincore/fincorebank/FincorebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincore.fincorebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincorebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
