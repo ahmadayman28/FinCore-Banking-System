550fd770010506a4067dca761d62ff98a48d755a
Ahmed Ayman <ahmed.ayman@example.com>
2026-08-09 11:24:04 +0300

    updated main page

--- src/main/java/com/fincore/fincorebank/FincorebankApplication.java
+++ src/main/java/com/fincore/fincorebank/FincorebankApplication.java
@@ -1,17 +1,8 @@
 package com.fincore.fincorebank;
 
-import java.beans.BeanProperty;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import com.fincore.fincorebank.auth_users.entity.User;
-import com.fincore.fincorebank.enums.NotificationType;
-import com.fincore.fincorebank.notification.dtos.NotificationDTO;
-import com.fincore.fincorebank.notification.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
 
